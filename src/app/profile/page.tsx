173e50a2427adbdde6ffc2e48f4171d5599e43a7
abdullah al noman <kazinoman9999@gmail.com>
2024-10-11 01:03:19 +0600

    authentication part is done

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import { logout } from "../login/actions";
+
+import { createClient } from "../../utils/server";
+
+export default async function ProfilePage() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
+  return (
+    <div>
+      ProfilePage {data.user.email}
+      <div>
+        <form action={logout}>
+          <button>Sign out</button>
+        </form>
+      </div>
+    </div>
+  );
+}
